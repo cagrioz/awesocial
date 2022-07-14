c995e2e914788f3b945f5fb0e4077aec726767d9
Cagri Oz <cagriozarpaciii@hotmail.com>
2022-07-14 10:02:14 +0300

    sidebar leftbar and post

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Button } from '@mui/material';
+import { Box, Stack } from '@mui/material';
+import Feed from './components/Feed';
+import Navbar from './components/Navbar';
+import Rightbar from './components/Rightbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
     return (
-        <div className="App">
-            <Button variant="text">Text</Button>
-            <Button variant="contained">Contained</Button>
-            <Button variant="outlined">Outlined</Button>
-        </div>
+        <Box className="App">
+            <Navbar />
+            <Stack direction="row" spacing={2} justifyContent="space-between">
+                <Sidebar />
+                <Feed />
+                <Rightbar />
+            </Stack>
+        </Box>
     );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1760a5',
+            light: '#3f8ebc',
+        },
+
+        secondary: {
+            main: '#f33',
+        },
+    },
+});
